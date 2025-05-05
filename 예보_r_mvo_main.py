4fcd89a14c312b6d3750ffac678a1dae8e4f973e
yjshin13 <124946810+yjshin13@users.noreply.github.com>
2025-05-06 03:45:04 +0900

    Update 예보_r_mvo_main.py

--- 예보_r_mvo_main.py
+++ 예보_r_mvo_main.py
@@ -143,6 +143,12 @@ if file is not None:
             with col_y:
                 st.info("Expected Risk: " + str(round(st.session_state.EF.loc[Target_index]["STDEV"] * 100, 2)) + "%")
 
+            with col_xx:
+                st.info("Expected Return: " + str(round(st.session_state.mean_er.loc[Target_index]["EXP_RET"] * 100, 2)) + "%")
+
+            with col_yy:
+                st.info("Expected Risk: " + str(round(st.session_state.std_er.loc[Target_index]["STDEV"] * 100, 2)) + "%")
+
             st.write("")
 
             Target_Weight = st.session_state.EF.loc[Target_index] \
